c6530b1575f076e015de4fb1e514f3ae2878bec8
Jensen Khemchandani <jensenkhem@Jensens-MacBook-Pro.local>
2024-06-04 22:19:17 -0600

    Add vue frontend project base, add string representation for an exercise

--- app/tracker/models.py
+++ app/tracker/models.py
@@ -3,6 +3,13 @@ from django.db import models
 # Create a sample model for the fitness tracker
 # Not sure if this is the best solution yet, just getting something out there
 class Exercise(models.Model):
+    # Attributes
     name = models.CharField("Name", max_length=200)
     pub_date = models.DateTimeField("Date Published")
     reps = models.IntegerField("Reps")
+
+    # Str representation
+    def __str__(self):
+        return "{name: " + self.name + ", reps: " + str(self.reps) + "}"
+
+
